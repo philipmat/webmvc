749e79becc61f854f51c411585d455ff5e040815
Philip Mateescu <github@philipm.us>
2012-01-12 13:46:09 -0600

    simplified object

--- static/organizations/kn-helper.js
+++ static/organizations/kn-helper.js
@@ -1,10 +1,9 @@
-window['organizations'] = (function() {
-	var me = {};
-	me.ViewModel = new (function() {
-		this.active = ko.observable();
-		this.elements = ko.observableArray([]);
-	})();	
-	me.makeModel = function (fromJson) {
+window['organizations'] = {
+	ViewModel : {
+		active : ko.observable(),
+		elements : ko.observableArray([])
+	}, 
+	makeModel : function (fromJson) {
 		var model = {};
 		model.id = fromJson.id;
 		model.name = ko.observable(fromJson.name);
@@ -13,22 +12,21 @@ window['organizations'] = (function() {
 			return '?/organizations/' + model.id;
 		});
 		return model;
-	};
-	me.loadModel = function (fromJson) {
+	},
+	loadModel : function (fromJson) {
 		var model = this.makeModel(fromJson);
 		this.ViewModel.active(model);
-	};
-	me.loadViewModel = function(fromJson) {
+	},
+	loadViewModel : function (fromJson) {
 		fromJson.all.forEach(function(o) {
 			var m = this.makeModel(o);
 			this.ViewModel.elements.push(m);
 		}, this);
-	};
-	me.testBind = function() {
+	},
+	testBind : function() {
 		var j0 = { id : 100, name : 'Moof', url : 'about:blank' },
 			j1 = { id : 101, name : 'Spoof', url : 'about:blank' };
 		var k = { all : [ j0, j1] };
 		this.loadViewModel(k);
-	};
-	return me;
-})();
+	}
+}
